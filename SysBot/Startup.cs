064a6917bd1313702d4a47b49d4b5b03ed5ebfca
Alexey Sys <mrdminyt@gmail.com>
2022-07-28 22:34:13 +0300

    Add startup

---
+++ SysBot/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Discord.WebSocket;
+
+namespace SysBot
+{
+    class Startup
+    {
+        private static DiscordSocketClient _client;
+
+        static void Main() => new Startup().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+        }
+    }
+}
